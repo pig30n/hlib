5f55df6452db99414ebcd5aeeb4db507e14bf6ca
Daniel Huebleitner <daniel@huebleitner.net>
2016-10-24 14:51:52 +0200

    add Stack

---
+++ incl/data/stack.h
@@ -0,0 +1,16 @@
+#ifndef H_LIB_STACK_H
+#include "hlib/incl/data/types.h"
+
+typedef struct stack Stack;
+
+struct stack *alloc_Stack(const Int64 max);
+Void free_stack(Stack **stack);
+Int8 peak_stack(const Stack *stack, Void** data);
+Int8 pop_stack(Stack *stack, Void **data);
+Int8 push_stack(Stack *stack, const Void *data);
+Int8 print_stack(const File *stream, Stack *stack, const Char *format);
+Int64 stack_get_cnt(const Stack *stack);
+Int64 stack_get_max(const Stack *stack);
+
+#define H_LIB_STACK_H
+#endif /*H_LIB_STACK_H*/
---
+++ src/data/stack.c
@@ -0,0 +1,76 @@
+#include "hlib/incl/data/stack.h"
+#include "hlib/incl/data/types.h"
+
+struct stack{
+     Int64 const max, cnt;
+     Void **data;
+};
+
+struct stack *alloc_Stack(const Int64 max)
+{
+     struct stack *stack = malloc(sizeof(struct stack));
+     stack->data = malloc(sizeof(Void*));
+     stack->cnt = 0;
+     stack->max = max;
+     return stack;
+}
+
+Void free_stack(struct stack **stack)
+{
+     if(*stack)
+          free(stack->data);
+     free(*stack);
+     *stack = NULL;
+}
+
+Int8 peak_stack(const struct stack *s, Void** data)
+{
+     if(!stack) return 1;
+     if(stack->cnt>=stack->max) return 2;
+     if(!data) return 3;
+
+     *data = stack->data[stack->cnt];
+     return 0;
+}
+
+Int8 pop_stack(struct stack *s, Void **data)
+{
+     if(!stack) return 1;
+     if(stack->cnt>=s->max) return 2;
+     if(!data) return 3;
+
+     *data = stack->data[(stack->cnt)--];
+     return 0;
+}
+
+Int8 push_stack(struct stack *stack, const Void *data)
+{
+     if(!stack) return 1;
+     if(stack->cnt>=stack->max) return 2;
+     if(!data) return 3;
+
+     stack->data[(stack->cnt)++] = data;
+     return 0;
+}
+
+Int8 print_stack(const File *stream, struct stack *stack, const Char *format)
+{
+     if(!stack) return = 1;
+     for(Int64 i=0; i<stack->cnt; i++)
+          if(fprintf(stream, format, *stack->data[i])<0) return 4;
+     return 0;
+}
+
+Int64 stack_get_cnt(const struct stack *stack)
+{
+     if(stack)
+          return stack->cnt;
+     return 0;
+}
+
+Int64 stack_get_max(const struct stack *stack)
+{
+     if(stack)
+          return stack->max;
+     return 0;
+}
